e0f4451f9739ee783d868928aa34b37f2802faf6
arojo <rojoadrian16@gmail.com>
2025-12-29 15:01:18 -0600

    Form upload styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,22 @@
-import react from "react";
+import React from "react";
+import "../Style.css";
 
-export default function Home(){
-    return(
-        <div>
-            <p>Home</p>
-        </div>
-    )
-};
+export default function Home() {
+  return (
+    <div className="pageWrapper">
+      <div className="formContainer">
+        <form className="uploadForm">
+          <h2>Upload your proposal</h2>
+          <p className="subtitle">Select a file to upload.</p>
+
+          <label className="fileInputLabel">
+            <input type="file" />
+            <span>Choose File</span>
+          </label>
+
+          <button type="submit">Upload</button>
+        </form>
+      </div>
+    </div>
+  );
+}
