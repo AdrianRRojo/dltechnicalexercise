63b47837ee220cf3d5eb7d23e0104431a8f9abc8
arojo <rojoadrian16@gmail.com>
2026-01-04 23:30:03 -0600

    invitation to bid system implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Review from "./pages/Review";
+import Bid from "./pages/Bid";
 import Thankyou from "./pages/Thankyou";
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/review" element={<Review />} />  
+      <Route path="/bid" element={<Bid />} />
       <Route path="/confirm" element={<Thankyou />}/>
     </Routes>
   );
